a12038cb2de2fd8c4a3475e7e21ca2cbb07e516f
Sahil Kapoor <sahil.kapoor@markitserv.com>
2016-03-16 15:51:33 +0530

    base commit

--- base/src/main/java/com/foozup/common/Validators.java
+++ base/src/main/java/com/foozup/common/Validators.java
@@ -1,5 +1,35 @@
 package com.foozup.common;
 
+import java.util.regex.Pattern;
+
 public class Validators {
 
+	public boolean validateEmail(String emailId){
+		String EMAIL_REGIX = "^[\\\\w!#$%&’*+/=?`{|}~^-]+(?:\\\\.[\\\\w!#$%&’*+/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\\\\.)+[a-zA-Z]{2,6}$";
+        Pattern pattern = Pattern.compile(EMAIL_REGIX);
+        java.util.regex.Matcher matcher = pattern.matcher(emailId);
+        return ((!emailId.isEmpty()) && (emailId!=null) && (matcher.matches()));
+		
+	}
+	
+	public boolean isValidNonEmptyString(String str){
+		if(null!=str && !str.isEmpty()){
+			return true;
+		}else{
+			return false;
+		}
+	}
+	
+	
+	public boolean checkLengthString(int len, String str){
+		if(isValidNonEmptyString(str)){
+			if(str.length()==len){
+				return true;
+			}else{
+				return false;
+			}
+		}else{
+			return false;
+		}
+	}
 }
